b065cb784196367b67a2ea742c53e074c18f4ad3
kaymei.leong <leongkaymei@gmail.com>
2025-06-16 21:28:17 +0800

    fix: border laws

--- src/components/sections/Laws.tsx
+++ src/components/sections/Laws.tsx
@@ -30,10 +30,10 @@ const Project: React.FC<ProjectProps> = ({ title, imageUrl, index, classNames })
 
 const Laws: React.FC = () => {
   return (
-    <section id="laws" className="min-h-screen w-full bg-amber-300 md:p-16 text-white">
+    <section id="laws" className="min-h-screen bg-amber-300 md:p-16 text-white">
       <div className="container mx-auto">
         <h2 className="font-titan -rotate-6 text-black text-4xl md:text-5xl lg:text-6xl font-bold pt-4 md:mb-16 text-center">LAW OF VIBES</h2>
-        <div className="md:border-8 md:-rotate-6 md:border-black md:border-solid">
+        <div className="md:border-8 md:border-black md:border-solid ">
           <Project 
             title='law_one'
             imageUrl={law1}
@@ -56,7 +56,7 @@ const Laws: React.FC = () => {
               title='law_four'
               imageUrl={law4}
               index={4}
-              classNames='right-0 md:pb-8 pb-4'
+              classNames='right-0 pb-8'
           />
         </div>
       </div>
